bc4eb7a266d777c4b2f11be13c49342ffab610a6
GokhanGKHN <gokhan.namli@outlook.com>
2023-04-27 13:31:50 +0300

    27042023

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Atama ve İşlemli atama
+
+       /*     int  x =3;
+            int  y=3;
+        Console.WriteLine(y);
+            y    +=2;
+         Console.WriteLine(y);    
+            y    /=1;
+
+        Console.WriteLine(y);    
+            x    *=2;
+        Console.WriteLine(x);
+           Console.ReadKey();*/
+
+
+       
+
+        int deneme =1;
+        int sonuc=0;    
+        sonuc=++deneme;
+        Console.WriteLine(sonuc);
+        Console.ReadKey();
+        int sonuc2=deneme;
+        //Console.Write(sonuc2);
+        //Console.ReadKey();
+        }
+    }
+}
